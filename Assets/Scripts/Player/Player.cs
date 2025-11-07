088630e383e95b124bcb0cfffb239fb6c3f8fc02
Peter <peterjiatong@gmail.com>
2025-11-06 23:31:01 -0800

    cancel backward steering feature

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -242,10 +242,10 @@ public class SimpleCarController : MonoBehaviour
                     break;
             }
         }
-        if (forwardVel < 0)
-        {
-            steer *= -1f;
-        }
+        // if (forwardVel < 0)
+        // {
+        //     steer *= -1f;
+        // }
 
         Transform rearLights = transform.Find("lights/rear");
         if (braking || (Input.GetKey(KeyCode.S) && player0) || (Input.GetKey(KeyCode.DownArrow) && !player0)) for (int i = 0; i < 2; i++) rearLights.GetChild(i).GetComponent<Light>().intensity = 25;
